62720a5b3eb79dcf4f59cf752f7686cf6e28dd4d
TseringDong <tseringlamadong@gmail.com>
2021-09-21 17:50:52 +0545

    learning of JS datatypes

---
+++ index.js
@@ -0,0 +1,43 @@
+// let a = 1; // Number
+// let b = "Hello World"; //String
+// let c = true; //boolean
+// let d = 1.5 //Float
+// // let e = Date.now();
+
+// console.log(a);
+
+// a = "IIMS";
+
+// console.log(a);
+
+// const example = "Hello World";
+
+// console.log(example);
+
+// // example = "IIMS";
+
+// // console.log(example);
+
+// let iims = null;
+// console.log(iims);
+
+
+let person = {
+    name: "Tendi",
+    age: 24,
+    address: "Boudha",
+    location: {
+        lat: 21.71,
+        lng: 85.32
+    }
+}
+console.log("This person's name is: ",person.name);
+console.log("His address is:", person.address);
+console.log("His date of birth is:", person.birthDate);
+console.log("His location Latitude is:", person.location.lat);
+console.log("His location Longitude is:", person.location.lng);
+
+
+let exampleArray = [1, 2, 3, 4, 5];
+
+console.log(exampleArray);
